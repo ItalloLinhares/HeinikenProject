3d2e761eaf8f39f184bce0ca968bc2bcaf0468a0
ItalloLinhares <45083049+ItalloLinhares@users.noreply.github.com>
2023-04-23 15:23:05 -0300

    Setup

---
+++ src/main/java/com/example/HeinikenProject/HeinikenProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.HeinikenProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeinikenProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeinikenProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HeinikenProject/HeinikenProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HeinikenProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeinikenProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
